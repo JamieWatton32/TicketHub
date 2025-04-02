a9131e51f65183711afd72fac2ddb0cdaf18367e
JamieWatton32 <jamiewatton199559955@gmail.com>
2025-04-02 15:01:37 -0300

    fixes

--- Ticket.cs
+++ Ticket.cs
@@ -5,7 +5,7 @@ namespace TicketHub {
         public int ConcertId { get; set; } = 0;
 
         [EmailAddress]
-        [Required(AllowEmptyStrings =false, ErrorMessage ="Email is required")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email is required")]
         public string Email { get; set; } = string.Empty;
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "Name is required")]
@@ -16,7 +16,7 @@ namespace TicketHub {
         public string Phone { get; set; } = string.Empty;
 
         [Required]
-        [Range(1,10,ErrorMessage = "Quanity of tickets must be between 1 and 10")]
+        [Range(1, 10, ErrorMessage = "Quantity of tickets must be between 1 and 10")]
         public int Quantity { get; set; } = 0;
 
         [Required]
@@ -24,11 +24,11 @@ namespace TicketHub {
         public string CreditCard { get; set; } = string.Empty;
 
         [Required]
-        [RegularExpression("^(0[1 - 9]|1[0 - 2])\\/?([0 - 9]{2})$", ErrorMessage = "Invalid expiration date")] 
+        [RegularExpression(@"^(0[1-9]|1[0-2])\/?([0-9]{2})$", ErrorMessage = "Expiration must be in MM/YY format.")]
         public string Expiration { get; set; } = string.Empty;
 
         [Required]
-        [RegularExpression("^\\d{3,4}$",ErrorMessage = "Invalid security code")]
+        [RegularExpression(@"^\d{3,4}$", ErrorMessage = "Invalid security code")]
         public string SecurityCode { get; set; } = string.Empty;
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "Address is required")]
@@ -41,7 +41,7 @@ namespace TicketHub {
         public string Province { get; set; } = string.Empty;
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "Postal Code is required")]
-        [RegularExpression("^[A-Z]\\d[A-Z] \\d[A-Z]\\d$",ErrorMessage = "Invalid postal code format")]
+        [RegularExpression(@"^[A-Z]\d[A-Z] ?\d[A-Z]\d$", ErrorMessage = "Invalid postal code format")]
         public string PostalCode { get; set; } = string.Empty;
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "Country is required")]
